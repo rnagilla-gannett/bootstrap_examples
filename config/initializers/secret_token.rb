a7e1956b841ae9c40ae5daf12220cb2bbc300930
rnagilla <rnagilla.gannett@gmail.com>
2012-03-07 09:35:50 -0500

    Initial code push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapExamples::Application.config.secret_token = 'cb11265cca755cb3758b58a749b6f78d1c44dc504b022b869f957d0b54762879cdcbdf7b49483abe9978f014b37b43d10077a8006ea49f76ad6200c7f835e06f'
